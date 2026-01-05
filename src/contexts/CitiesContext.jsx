5baa495378baa22906c95dc2bb5b1aee1589514c
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2026-01-05 21:55:52 +0200

    Context test again

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,7 +7,6 @@ const BASE_URL = "http://localhost:9000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -24,29 +23,8 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
-
-  async function getCity(id) {
-    try {
-      setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      setCurrentCity(data); // data = array
-    } catch {
-      alert("there is an error Loading data...");
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
   return (
-    <CitiesContext.Provider
-      value={{
-        cities,
-        isLoading,
-        currentCity,
-        getCity,
-      }}
-    >
+    <CitiesContext.Provider value={{ cities, isLoading }}>
       {children}
     </CitiesContext.Provider>
   );

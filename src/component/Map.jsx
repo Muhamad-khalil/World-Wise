40d2a84acab76652915a5a313f5eb87b12d797fe
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2026-01-05 19:13:41 +0200

    Finishing the City View and Including a Map With the Leaflet Library

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,8 +1,11 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParam, setSearchParam] = useSearchParams();
   const lat = searchParam.get("lat");
@@ -15,13 +18,22 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <h1>
-        Position : lat{lat} lng{lng}
-      </h1>
-      <button onClick={() => setSearchParam({ lat: 23, lng: 20 })}>
-        Change Poss
-      </button>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
